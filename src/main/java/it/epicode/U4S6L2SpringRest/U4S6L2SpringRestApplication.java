ea4c8ab39ce401f7c5775c775c2f89815ae76917
Alice Lazzeri <alice.lazzeri@hotmail.it>
2024-05-21 15:00:41 +0200

    Initial commit

---
+++ src/main/java/it/epicode/U4S6L2SpringRest/U4S6L2SpringRestApplication.java
@@ -0,0 +1,13 @@
+package it.epicode.U4S6L2SpringRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U4S6L2SpringRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U4S6L2SpringRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/epicode/U4S6L2SpringRest/U4S6L2SpringRestApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.U4S6L2SpringRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U4S6L2SpringRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
